9ef449edfff84c4b5d161e81b4392f9590211bae
Anastasia Khizgiyaev <nastiakh28@gmail.com>
2024-04-09 12:18:26 +0300

    finising touches

--- src/Words.js
+++ src/Words.js
@@ -1,4 +1,7 @@
-export const emptyTable = [
+
+import wordBank from "./wordle-bank.txt";
+
+export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -6,3 +9,16 @@ export const emptyTable = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      wordSet = new Set(wordArr);
+    });
+  return { wordSet, todaysWord };
+};
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,20 +1,26 @@
 import React, { useContext } from "react";
 import { AppContext } from "../App";
 
-function Key({ keyVal, bigKey }) {
-  const { onEnter, onDelete, onLetter } = useContext(AppContext);
+function Key({ keyVal, bigKey, disabled }) {
+  const { gameOver, onSelectLetter, onDelete, onEnter } =
+    useContext(AppContext);
 
   const selectLetter = () => {
+    if (gameOver.gameOver) return;
     if (keyVal === "ENTER") {
       onEnter();
     } else if (keyVal === "DELETE") {
       onDelete();
     } else {
-      onLetter(keyVal);
+      onSelectLetter(keyVal);
     }
   };
   return (
-    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+    <div
+      className="key"
+      id={bigKey ? "big" : disabled && "disabled"}
+      onClick={selectLetter}
+    >
       {keyVal}
     </div>
   );
